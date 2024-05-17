1024b6e1cfd0e5855831124719a7f58819841d34
joaquingutierrez <joaquinguty@gmail.com>
2024-05-17 19:31:35 -0300

    Maquetado y estilos para Contact desktop

--- src/componentContainer/Contact/index.jsx
+++ src/componentContainer/Contact/index.jsx
@@ -4,19 +4,19 @@ import video from "../../assets/videos/contact-video.mp4"
 const Contact = () => {
     return (
         <main className="contactMain">
-            <div>
-                <h2><span>YOUR ADVENTURE</span> IS JUST A STEP AWAY</h2>
+            <div className="contactContent">
+                <h2><span className="yellowColor">YOUR ADVENTURE</span> IS JUST A STEP AWAY</h2>
                 <h3>Contact us and live the experience.</h3>
                 <form className="contactForm">
-                    <input type="text" name="fullName" id="fullName" placeholder="Full Name" />
-                    <input type="email" name="email" id="email" placeholder="Email Adress" />
-                    <textarea name="textArea" id="textArea">Message</textarea>
-                    <div>
+                    <input className="contact-textInput" type="text" name="fullName" id="fullName" placeholder="Full Name" />
+                    <input className="contact-textInput" type="email" name="email" id="email" placeholder="Email Adress" />
+                    <textarea className="contact-textAreaInput" name="textArea" id="textArea" placeholder="Message"></textarea>
+                    <div className="contact-checkboxContainer">
                         <input type="checkbox" name="" id="" />
                         <p>By submitting this form you agree to send your informationto Futbol America . We will use it
                             to support our commercial activity and send your newsletters according to our pricavy policy.</p>
                     </div>
-                    <input type="submit" value="Submit" />
+                    <input type="submit" value="Submit" className="contact-submitButton"/>
                 </form>
             </div>
             <video autoPlay muted loop id="contactVideo">
