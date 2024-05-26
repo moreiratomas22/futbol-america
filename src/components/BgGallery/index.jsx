65e5f7987b728fc0fb95d9c6598ed8f0b664fc6c
joaquingutierrez <joaquinguty@gmail.com>
2024-05-26 19:08:27 -0300

    Fix: Ahora la galeria BgGallery tiene animaciones

--- src/components/BgGallery/index.jsx
+++ src/components/BgGallery/index.jsx
@@ -4,40 +4,47 @@ import "./style.css"
 const BgGallery = ({ info }) => {
 
     const [items, setItems] = useState(info)
+    const [animation, setAnimation] = useState(false)
 
     const handleClick = () => {
-        setItems(prev => {
-            const newItems = [...prev]
-            newItems.push(newItems[0])
-            newItems.splice(0,1)
-            return newItems
-        })
+        setAnimation(true)
+        setTimeout(() => {
+            setItems(prev => {
+                setAnimation(false)
+                const newItems = [...prev]
+                newItems.push(newItems[0])
+                newItems.splice(0, 1)
+                return newItems
+            })
+        }, 200)
     }
 
     return (
-        <div className="bgGallety-container">
-            <div className="bgGallety-bgContainer">
+        <div className="bgGallery-container">
+            <div className="bgGallery-bgContainer">
                 {items.map((item, index) => {
-                    return <img key={index} src={item.img} alt="" />
+                    return <img className={animation ? "bgGallery-bgImg-off" : ""} key={index} src={item.img} alt="" />
                 })}
             </div>
-            <div className="bgGallety-contentContainer">
-                <div className="bgGallety-content">
+            <div className="bgGallery-contentContainer">
+                <div className="bgGallery-content">
                     {items.map((item, index) => {
-                        return <div className="bgGallety-info" key={index}>
-                                    <h2>{item.title}</h2>
-                                    <p>{item.desc}</p>
-                                </div>
+                        return (
+                            <div className={`bgGallery-info ${animation ? "bgGallery-info-animation-up bgGallery-transition " : ""}`} key={index}>
+                                <h2>{item.title}</h2>
+                                <p>{item.desc}</p>
+                            </div>
+                        )
                     })}
                 </div>
-                <div className="bgGallety-imgButtonsContainer">
-                    <div className="bgGallety-imgButton1">
+                <div className="bgGallery-imgButtonsContainer">
+                    <div className={`${animation ? "bgGallery-imgButton0 bgGallery-transition" : "bgGallery-imgButton1"}`}>
                         <img src={items[0].img} alt="" />
                     </div>
-                    <div className="bgGallety-imgButton2">
+                    <div className={`${animation ? "bgGallery-imgButton1 bgGallery-transition" : "bgGallery-imgButton2"}`}>
                         <img onClick={handleClick} src={items[1].img} alt="" />
                     </div>
-                    <div className="bgGallety-imgButton3">
+                    <div className={`${animation ? "bgGallery-imgButton2 bgGallery-transition" : "bgGallery-imgButton3"}`}>
                         <img onClick={handleClick} src={items[0].img} alt="" />
                     </div>
                 </div>
