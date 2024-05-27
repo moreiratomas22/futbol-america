a1297850dc8a2a3cd048a3701122f181e5092926
joaquingutierrez <joaquinguty@gmail.com>
2024-05-27 17:41:57 -0300

    Fix: Agregado titulo a testimonies y arreglado algunos problemas con el overflow scroll de algunas partes de services

--- src/componentContainer/Services/index.jsx
+++ src/componentContainer/Services/index.jsx
@@ -35,7 +35,7 @@ const Services = () => {
     const testimony2 = {
         title: '"Dream Come True!"',
         stars: 5,
-        desc: '"If you' + "'" + 're a soccer enthusiast looking to immerse yourself in the rich tapestry of Argentine soccer culture while exploring the beautiful sights of the country, then Futbol America' +"'" + 's tour is an absolute must! From the moment we landed, the tour was flawlessly organized, with each day packed full of activities that catered to both the passionate soccer fan and the avid traveler. Watching games in some of Argentina'+ "'" + 's most storied stadiums, we felt the pulsating energy of the crowds and the raw passion for the beautiful game. But Futbol America' +"'" + 's experience goes far beyond just watching soccer; they arranged for us to play friendly matches with local teams, providing a rare and thrilling chance to truly engage with the sport at a grassroots level. The camaraderie and sportsmanship displayed were heartwarming, creating memories that will last a lifetime. In addition to the soccer-filled agenda, Futbol America did a phenomenal job of integrating cultural experiences. We explored vibrant Buenos Aires. Each activity was thoughtfully chosen to ensure we got a holistic view of Argentina’s diverse and rich cultural heritage. The tour guides provided by Futbol America were exceptional—knowledgeable, friendly, and always willing to go the extra mile to make our experience special. Their insights into the history of soccer in Argentina enriched our understanding and appreciation of each match and soccer event we attended. Overall, Futbol America offered an unforgettable journey that perfectly balanced soccer, sightseeing, and cultural immersion. Whether you' + "'" + 're a die-hard soccer fan or just someone keen to explore Argentina through the lens of its favorite sport, this tour delivers a unique and exhilarating experience. We came as strangers but left as part of a community, united by our shared passions and unforgettable experiences. Hats off to Futbol America for arranging such a spectacular adventure and truly a trip of a lifetime!"',
+        desc: '"If you' + "'" + 're a soccer enthusiast looking to immerse yourself in the rich tapestry of Argentine soccer culture while exploring the beautiful sights of the country, then Futbol America' + "'" + 's tour is an absolute must! From the moment we landed, the tour was flawlessly organized, with each day packed full of activities that catered to both the passionate soccer fan and the avid traveler. Watching games in some of Argentina' + "'" + 's most storied stadiums, we felt the pulsating energy of the crowds and the raw passion for the beautiful game. But Futbol America' + "'" + 's experience goes far beyond just watching soccer; they arranged for us to play friendly matches with local teams, providing a rare and thrilling chance to truly engage with the sport at a grassroots level. The camaraderie and sportsmanship displayed were heartwarming, creating memories that will last a lifetime. In addition to the soccer-filled agenda, Futbol America did a phenomenal job of integrating cultural experiences. We explored vibrant Buenos Aires. Each activity was thoughtfully chosen to ensure we got a holistic view of Argentina’s diverse and rich cultural heritage. The tour guides provided by Futbol America were exceptional—knowledgeable, friendly, and always willing to go the extra mile to make our experience special. Their insights into the history of soccer in Argentina enriched our understanding and appreciation of each match and soccer event we attended. Overall, Futbol America offered an unforgettable journey that perfectly balanced soccer, sightseeing, and cultural immersion. Whether you' + "'" + 're a die-hard soccer fan or just someone keen to explore Argentina through the lens of its favorite sport, this tour delivers a unique and exhilarating experience. We came as strangers but left as part of a community, united by our shared passions and unforgettable experiences. Hats off to Futbol America for arranging such a spectacular adventure and truly a trip of a lifetime!"',
         img: pittman,
         name: "Ella Pittman",
         job: ""
@@ -49,10 +49,13 @@ const Services = () => {
             <InfiniteGallery />
             <HotelSectionContainer />
             <Schedules />
-            <div className="services-testimoniesContainer">
-                <Testimony content={testimony1}/>
-                <Testimony content={testimony2}/>
-            </div>
+            <section className="services-testimoniesSection">
+                <h2>Hear their <span>Stories</span></h2>
+                <div className="services-testimoniesContainer">
+                    <Testimony content={testimony1} />
+                    <Testimony content={testimony2} />
+                </div>
+            </section>
         </main>
     )
 }
