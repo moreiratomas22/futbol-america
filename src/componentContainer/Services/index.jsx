8f8eae51fa005114310fcadb48080b687771508f
joaquingutierrez <joaquinguty@gmail.com>
2024-05-27 16:06:50 -0300

    Cambiados los testimonios

--- src/componentContainer/Services/index.jsx
+++ src/componentContainer/Services/index.jsx
@@ -5,9 +5,8 @@ import brazilImg from "../../assets/images/bgGallery/brazil.png"
 import AccordionContainer from "../AccordionContainer"
 import AutoGalleryContainer from "../AutoGalleryContainer"
 import HotelSectionContainer from "../HotelSectionContainer"
-import emilyRodriguezImg from "../../assets/testimonies/emilyRodriguez.png"
-import carlosGomezImg from "../../assets/testimonies/carlosGomez.png"
-import michaelSmithImg from "../../assets/testimonies/michaelSmith.png"
+import shermanImg from "../../assets/testimonies/sherman.jpg"
+import pittman from "../../assets/testimonies/pittman.jpg"
 
 const Services = () => {
 
@@ -27,28 +26,19 @@ const Services = () => {
     const testimony1 = {
         title: '"Unforgettable Experience!"',
         stars: 5,
-        desc: '"Our university soccer team had the most incredible time with Futbol America. From the moment we arrived in Argentina, everything was taken care of seamlessly. The training sessions with top division clubs were invaluable, and the cultural experiences were unforgettable. Highly recommend!"',
-        img: emilyRodriguezImg,
-        name: "Emily Rodriguez",
-        job: "Coach, University of California Soccer Team"
+        desc: '"Visiting Argentina with Futbol America tours was an experience of a lifetime. I got to play the beautiful sport in a different way. I experienced  a completely new culture and lifestyle. Getting to train with world class players was really something special. Touring throughout La plata and visiting in Buenos Aires was something I’ll never forget. It was really amazing to visit stadiums such as BOCA and Estudiantes. The highlight of the trip was the opportunity to watch the Copa Argentina semifinal between Defensa y Justicia and San Lorenzo. It let me understand how passionate the fans are about the game they love. The trip was truly a wonderful experience and would certainly recommend futbol America tours."',
+        img: shermanImg,
+        name: "Sam Sherman",
+        job: ""
     }
 
     const testimony2 = {
         title: '"Dream Come True!"',
         stars: 5,
-        desc: '"Traveling to Brazil with Futbol America was a dream come true for our team. The opportunity to train with Brazilian clubs was beyond anything we could have imagined. The staff was friendly, knowledgeable, and truly went above and beyond to make our trip unforgettable. Can' + "'" + 't wait to do it again!"',
-        img: carlosGomezImg,
-        name: "Carlos Gomez",
-        job: "Player, Florida State University Soccer Team"
-    }
-
-    const testimony3 = {
-        title: '"Exceptional Service!"',
-        stars: 5,
-        desc: '"The attention to detail in planning our trip was evident, and every aspect exceeded our expectations. The training sessions were challenging yet rewarding, and the cultural immersion was an added bonus. Highly recommend for any university!!!"',
-        img: michaelSmithImg,
-        name: "Michael Smith",
-        job: "Head Coach, Stanford University Soccer Team"
+        desc: '"If you' + "'" + 're a soccer enthusiast looking to immerse yourself in the rich tapestry of Argentine soccer culture while exploring the beautiful sights of the country, then Futbol America' +"'" + 's tour is an absolute must! From the moment we landed, the tour was flawlessly organized, with each day packed full of activities that catered to both the passionate soccer fan and the avid traveler. Watching games in some of Argentina'+ "'" + 's most storied stadiums, we felt the pulsating energy of the crowds and the raw passion for the beautiful game. But Futbol America' +"'" + 's experience goes far beyond just watching soccer; they arranged for us to play friendly matches with local teams, providing a rare and thrilling chance to truly engage with the sport at a grassroots level. The camaraderie and sportsmanship displayed were heartwarming, creating memories that will last a lifetime. In addition to the soccer-filled agenda, Futbol America did a phenomenal job of integrating cultural experiences. We explored vibrant Buenos Aires. Each activity was thoughtfully chosen to ensure we got a holistic view of Argentina’s diverse and rich cultural heritage. The tour guides provided by Futbol America were exceptional—knowledgeable, friendly, and always willing to go the extra mile to make our experience special. Their insights into the history of soccer in Argentina enriched our understanding and appreciation of each match and soccer event we attended. Overall, Futbol America offered an unforgettable journey that perfectly balanced soccer, sightseeing, and cultural immersion. Whether you' + "'" + 're a die-hard soccer fan or just someone keen to explore Argentina through the lens of its favorite sport, this tour delivers a unique and exhilarating experience. We came as strangers but left as part of a community, united by our shared passions and unforgettable experiences. Hats off to Futbol America for arranging such a spectacular adventure and truly a trip of a lifetime!"',
+        img: pittman,
+        name: "Ella Pittman",
+        job: ""
     }
 
     return (
@@ -62,7 +52,6 @@ const Services = () => {
             <div className="services-testimoniesContainer">
                 <Testimony content={testimony1}/>
                 <Testimony content={testimony2}/>
-                <Testimony content={testimony3}/>
             </div>
         </main>
     )
