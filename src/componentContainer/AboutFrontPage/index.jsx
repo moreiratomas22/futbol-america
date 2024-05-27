55b57681ec12de7e16638eb6b52cc5a25d1b5885
joaquingutierrez <joaquinguty@gmail.com>
2024-05-27 19:27:15 -0300

    Fix: Arreglado problema con algunas resoluciones en About

--- src/componentContainer/AboutFrontPage/index.jsx
+++ src/componentContainer/AboutFrontPage/index.jsx
@@ -8,8 +8,8 @@ const AboutFrontPage = () => {
         <section className="about-frontPage-container">
             <img src={frontImg} alt="" />
             <div className='about-frontPage-content'>
-                <h1 className='about-frontPage-title'>DISCOVER OUR <span>STORY</span></h1>
                 <div className='about-frontPage-leftTextContainer'>
+                    <h1 className='about-frontPage-title'>DISCOVER OUR <span>STORY</span></h1>
                     <p className='about-frontPage-text'>Our vision is to lead sports tourism in South America, guided by ethics, transparency, and social responsibility.</p>
                     <p className='about-frontPage-text'>Futbol America has strived to achieve success, working hand in hand with the best first division professional clubs in Brazil and Argentina, their national teams and amateur clubs. This gives us the flexibility to organize football activities for all levels and ages. We are equally aware of the increasing standards of our clients. Therefore, we will customize our program to meet the needs of each client. We coordinate all the games, fields, food, transportation, lodging and tourism. We recognize that only by providing consistently high-quality performance and service experience can we exceed these expectations and maintain long-term relationships based on trust. </p>
                 </div>
