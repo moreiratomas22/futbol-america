aead41eebad690da4c0625b748cdedcef6a6731b
joaquingutierrez <joaquinguty@gmail.com>
2024-06-02 21:50:55 -0300

    Responsive para tablet Services - Accordion

--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -23,13 +23,13 @@ const Accordion = ({ items }) => {
 
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         setWidthAcc(accordionRef.current.offsetWidth)
-    },[])
+    }, [])
 
     const calcPosition = (index) => {
-        const place = widthAcc/2
-        return place - ((11-index) * 80 )
+        const place = widthAcc / 2
+        return place - ((11 - index) * 80)
     }
 
 
@@ -40,28 +40,31 @@ const Accordion = ({ items }) => {
                 <p>Step into a world where soccer transcends boundaries and becomes a transformative journey of discovery and achievement. We're more than just a destination – we're your gateway to a realm where passion meets purpose, where players, coaches, and fans alike unite in the pursuit of greatness. With a steadfast commitment to excellence and innovation, we're here to empower you to unleash your full potential on and off the field. Our holistic approach to soccer development goes beyond the game itself, encompassing personalized mentorship, immersive experiences, and sustainable practices that shape the future of the sport.</p>
             </div>
             <div className="accordion-contentContainer">
-            <div ref={accordionRef} className={`accordion-content ${someOtherExpanded ? "accordion-content-move" : ""}`}>
-                {items.map((item, index) => {
-                    return (
-                        <div key={index} ref={el => itemRefs.current[index] = el} className={`accordion-item ${expandedIndex === index ? "accordion-item-expand" : ""} ${(expandedIndex !== index && someOtherExpanded) ? "accordion-item-disappear" : ""}`} style={{ zIndex: index, left: `${calcPosition(index)}px` }}>
-                            <div className="accordion-item-sideTitle">
-                                <div className="accordion-item-expandButtonContainer" onClick={() => handleClick(index)}>
-                                    <div className={`accordion-item-expandButton-verticalLine ${expandedIndex === index ? "accordion-item-expandButton-verticalLineisExpanded" : ""}`}></div>
-                                    <div className="accordion-item-expandButtonHotizontalLine"></div>
+                <div ref={accordionRef} className={`accordion-content ${someOtherExpanded ? "accordion-content-move" : ""}`}>
+                    {items.map((item, index) => {
+                        return (
+                            <div key={index} ref={el => itemRefs.current[index] = el} className={`accordion-item ${expandedIndex === index ? "accordion-item-expand" : ""} ${(expandedIndex !== index && someOtherExpanded) ? "accordion-item-disappear" : ""}`} style={{ zIndex: index, left: `${calcPosition(index)}px` }}>
+                                <div className="accordion-item-sideTitle">
+                                    <div className="accordion-item-expandButtonContainer" onClick={() => handleClick(index)}>
+                                        <div className={`accordion-item-expandButton-verticalLine ${expandedIndex === index ? "accordion-item-expandButton-verticalLineisExpanded" : ""}`}></div>
+                                        <div className="accordion-item-expandButtonHotizontalLine"></div>
+                                    </div>
+                                    <h4>{item.sideTitle}</h4>
                                 </div>
-                                <h4>{item.sideTitle}</h4>
+                                <div className="accordion-item-imgContainer">
+                                    <img src={item.img} alt="" />
+                                </div>
+                                <div className="accordion-item-textContainer">
+                                    <h3>{item.title}</h3>
+                                    <p>{item.text}</p>
+                                </div>
                             </div>
-                            <div className="accordion-item-imgContainer">
-                                <img src={item.img} alt="" />
-                            </div>
-                            <div className="accordion-item-textContainer">
-                                <h3>{item.title}</h3>
-                                <p>{item.text}</p>
-                            </div>
-                        </div>
-                    )
-                })}
-            </div>
+                        )
+                    })}
+                </div>
+                <div className="accordion-phone-contentContainer">
+
+                </div>
             </div>
         </section>
     )
